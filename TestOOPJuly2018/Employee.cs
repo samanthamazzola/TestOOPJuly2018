6b43fc7460f6bc0036b29d986062c1f47da92fd9
samantha mazzola <smazzola7@gmail.com>
2018-07-31 09:58:01 -0400

    upload class examples with constructors

---
+++ TestOOPJuly2018/Employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+
+
+namespace TestOOPJuly2018
+{
+    public class Employee
+    {
+      
+
+            #region Fields
+            // Fields to represent the data (private)
+            private string firstName;
+        private string lastName;
+        private float salary;
+        private string bonus;
+        private string ssn;
+        private string address;
+        #endregion
+
+
+        // Properties to protect the fields + validation (use properties to access this data) (Regex)
+        // Each property will be related to any of the above fields
+        // design a method to return first name
+        public string FirstName // like a method WITHOUT param parenthesis
+        {
+            set { firstName = value; } // value KEYWORD is a placeholder and will be changed with whatever the user provides
+            get { return firstName; } // return first name variable NOT the FirstName method
+        }
+        public string LastName // like a method WITHOUT param parenthesis
+        {
+            set { lastName = value; }
+            get { return lastName; }
+        }
+        public float Salary // like a method WITHOUT param parenthesis
+        {
+            set { salary = value; }
+            get { return salary; }
+        }
+        public string Bonus // like a method WITHOUT param parenthesis
+        {
+            set { bonus = value; }
+            get { return bonus; }
+        }
+        public string SSN // like a method WITHOUT param parenthesis
+        {
+            set { ssn = value; }
+            get { return ssn; }
+        }
+        public string Address
+        {
+            set { address = value; }
+            get { return address; }
+        }
+
+
+        // Constructors
+        // Method used to initialize data; will have the same name as the Class name
+        // can only be called when you create the object 
+        // every class has a default Constructor: C# will write one for you
+
+
+        // default constructor (no-argument constructor)
+        public Employee()  
+        // says instead of putting blanks, put these initliazed variables in 
+        {
+            FirstName = "not assigned";
+            LastName = "not assigned";
+            Salary = 40000;
+            SSN = "000-00-0000";
+        }
+
+
+        // Overloaded Constructor
+        public Employee(string fn, string ln, float sal, string s) // this one specifies string input
+        {
+            // firstName = fn; <----- gives user access to the private var
+            FirstName = fn;
+            LastName = ln;
+            Salary = sal;
+            SSN = s;
+            // fn = FirstName;  // logical error!! it's taking the user's input then assigning to the FirstName property
+        }
+
+
+        // Methods
+
+
+
+        }
+    }
+
---
+++ TestOOPJuly2018/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestOOPJuly2018
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            // List of Employees
+            // like a data entry program where you can add as many employees as you want 
+            List<Employee> employees = new List<Employee>(); //use the class Employees with the objects associated 
+
+            Employee s1 = new Employee("Damien", "Rocchi", 15000000, "xxx-xx-xxxx"); // uses constructors build in the Employee Class
+
+            // without constructor 
+            Employee s2 = new Employee();
+            s2.FirstName = "Damien";
+            s2.LastName = "Rocchi";
+            s2.Salary = 1500000;
+            s2.SSN = "xxx-xx-xxxx";
+
+
+            //used to add new employees to the list
+            while(true) // made an object
+            {
+                Employee temp = new Employee(); // make the object then modify based on the input
+                Console.WriteLine("Please provide the First Name:");
+                temp.FirstName = Console.ReadLine(); // temporary 
+
+                Console.WriteLine("Please provide the Last Name:");
+                temp.LastName = Console.ReadLine();
+
+                Console.WriteLine("Please provide the SSN:");
+                temp.SSN = Console.ReadLine();
+
+                Console.WriteLine("Please provide the Salary:");
+                temp.Salary = float.Parse(Console.ReadLine());
+
+                employees.Add(temp); // add the temp object to the employees list 
+
+                Console.WriteLine("Do you want to add more employees?");
+                string choice = Console.ReadLine();
+                if (choice == "N")
+                    break;
+                // if you want to add new info, you need to create a new object and set values
+            }
+
+            // you want print all employees
+            foreach (Employee e in employees) //e is a variable // gets from list!
+            {
+                Console.WriteLine($"{e.FirstName} {e.LastName}");
+                Console.WriteLine("======================");
+
+            }
+
+            // DOES THE SAME THING
+            //for (int i = 0; i < employees.Count; i++)  // pulls the info
+            //{
+            //    Console.WriteLine($"{employees[i]} {employees[i].LastName}");
+            //    Console.WriteLine("======================");
+            //}
+
+
+
+
+
+            //Employee john = new Employee(); // object and tells how it will behave 
+
+            //john.FirstName = "John"; // sets first "John" || then gets as your value
+
+            //Console.WriteLine(john.FirstName); // use get because you're trying to print what's already been set
+
+            //john.Salary = 5000; // set 
+
+            //john.Salary = john.Salary + 1000; // get set; getting the value, adding 1000 to it then setting it 
+
+            //Console.WriteLine(john.SSN); // cannot access. with a string the default value.. will print blank
+            // properties have defaults if not provided; string = blank; int = 0; bool = false
+
+            ////////////////////
+
+           
+
+
+
+
+        }
+    }
+}
+
+
